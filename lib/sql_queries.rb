e29e5b201874812e259df72079efd5b4fbe08db8
Romario Gustave <romagustave10@gmail.com>
2020-12-29 00:51:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,7 +6,7 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-ef selects_the_titles_of_all_projects_and_their_pledge_amounts
+def selects_the_titles_of_all_projects_and_their_pledge_amounts
   "SELECT projects.title, SUM(pledges.amount)
   FROM projects
   INNER JOIN pledges
